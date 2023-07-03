2fc97c833f6d66e4db81f90e88add9b1b93e94e9
Aymeric Pellé <aympelle@gmail.com>
2023-07-03 18:23:57 +0200

    v0.1.1: Add option --output-on-failure when running ctest.

--- cmake_test_helpers.sh
+++ cmake_test_helpers.sh
@@ -34,7 +34,7 @@ function cmake_test_full_build
 	command cmake -DBUILD_${pname}_TESTS=On -DBUILD_${pname}_EXAMPLES=On -D${pname}_BUILD_TESTS=On -D${pname}_BUILD_EXAMPLES=On -S $source_dir -B $build_dir \
 	&& command cmake --build $build_dir -j$core_count \
 	&& command ls -1 $build_dir \
-	&& command ctest --progress --test-dir $build_dir
+	&& command ctest --progress --output-on-failure --test-dir $build_dir
 	local return_val=$?
 	trace_function_end
 	return $return_val
@@ -78,7 +78,7 @@ function cmake_test_full_install
 	export CMAKE_PREFIX_PATH=/tmp/local
 	command cmake -DBUILD_${pname}_TESTS=On -DBUILD_${pname}_EXAMPLES=On -D${pname}_BUILD_TESTS=On -D${pname}_BUILD_EXAMPLES=On -S $source_dir -B $build_dir \
 	&& command cmake --build $build_dir -j$core_count \
-	&& command ctest --progress --test-dir $build_dir \
+	&& command ctest --progress --output-on-failure --test-dir $build_dir \
 	&& command cmake --install $build_dir --prefix $install_dir \
 	&& command tree -ifF -I 'build' $install_dir/
 	local return_val=$?
