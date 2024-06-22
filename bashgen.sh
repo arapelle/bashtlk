7626e11a96680c110d87d86288f6f9a0cbbafa6d
Aymeric Pellé <aympelle@gmail.com>
2024-06-22 12:19:32 +0200

    v0.3.0: Re-indent bashgen.sh.

--- bashgen.sh
+++ bashgen.sh
@@ -2,46 +2,46 @@
 
 function bashgen_script()
 {
-  (( $# == 0 )) && echofmt "{red}ERROR: Right call: bashgen_script script[.sh]" && return 1
-
-  script_path="$1"
-  script_dir=$(parentdir $script_path)
-  mkdir -p $script_dir
-  ext=$(fileext $script_path)
-  if [ "$ext" != "sh" ]
-  then
-    script_path="$script_path.sh"
-  fi
-  touch $script_path
-  chmod u+x $script_path
-  echo -e "#!/usr/bin/env bash\n" > $script_path
+    (( $# == 0 )) && echofmt "{red}ERROR: Right call: bashgen_script script[.sh]" && return 1
+  
+    script_path="$1"
+    script_dir=$(parentdir $script_path)
+    mkdir -p $script_dir
+    ext=$(fileext $script_path)
+    if [ "$ext" != "sh" ]
+    then
+      script_path="$script_path.sh"
+    fi
+    touch $script_path
+    chmod u+x $script_path
+    echo -e "#!/usr/bin/env bash\n" > $script_path
 }
 
 function bashgen_getopts
 {
 	echo \
-	"  local OPTIND
-  while getopts \"a:f\" opt;
-  do
-  case \$opt in
+	"local OPTIND
+while getopts \"a:f\" opt;
+do
+case \$opt in
     a)
-      echo \"OK: arg option enabled.\"
-      local value=\$OPTARG
-      if [[ \$value =~ ^-?[0-9]+\$ # value is integer
-            && \$value -gt 0 ]]
-      then
-        echo \"OK: arg is a positive integer\"
-      else
-        echo \"ERROR: arg must be a positive integer.\"
-      fi
-      ;;
+        echo \"OK: arg option enabled.\"
+        local value=\$OPTARG
+        if [[ \$value =~ ^-?[0-9]+\$ # value is integer
+              && \$value -gt 0 ]]
+        then
+            echo \"OK: arg is a positive integer\"
+        else
+            echo \"ERROR: arg must be a positive integer.\"
+        fi
+        ;;
     f)
-      echo \"OK: flag option enabled.\"
+        echo \"OK: flag option enabled.\"
     ;;
     ?)
-      echo \"ERROR: Unknown option.\"
+        echo \"ERROR: Unknown option.\"
     ;;
-  esac
-  done
-  shift \$((OPTIND-1))"
+esac
+done
+shift \$((OPTIND-1))"
 }
