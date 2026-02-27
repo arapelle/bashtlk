cc0491ab2192874e15d32f2fbf666eb91f68dfa2
Aymeric Pellé <aymeric.pelle.dev@proton.me>
2026-02-27 13:34:40 +0100

    WIP commandf (renamed command) takes options.
    -s
    -f verbose_format

--- command.sh
+++ command.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
 
-function commandf
-{
-	local format=$1
-	shift
-	format=${format//\{\*\}/$*}
-	echofmt $format
-	$*
-	return $?
-}
-export -f commandf
+export COMMAND_SIMULATION=0
 
 export COMMAND_DEFAULT_FORMAT="{bold}{green}> {*}"
 function command
 {
-    commandf "${COMMAND_DEFAULT_FORMAT}" "$*"
-    return $?
+	local format="${COMMAND_DEFAULT_FORMAT}"
+	local simulation=${COMMAND_SIMULATION:-0}
+	local OPTIND
+	while getopts "sf:" opt;
+	do
+	case $opt in
+		f)
+			format="$OPTARG"
+			;;
+		s)
+			simulation=1
+			;;
+		?)
+			echo "ERROR: Unknown option."
+			;;
+	esac
+	done
+	shift $((OPTIND-1))
+
+	format=${format//\{\*\}/$*}
+	echofmt "$format"
+	(( $simulation == 0 )) && $*
+	return $?
 }
 export -f command
 
 export SUBCOMMAND_DEFAULT_FORMAT="{bold}{blue}>> {*}"
 function subcommand
 {
-    commandf "${SUBCOMMAND_DEFAULT_FORMAT}" "$*"
+	local format="${SUBCOMMAND_DEFAULT_FORMAT}"
+	local simulation=""
+	local OPTIND
+	while getopts "sf:" opt;
+	do
+	case $opt in
+		f)
+			format="$OPTARG"
+			;;
+		s)
+			simulation="-s"
+			;;
+		?)
+			echo "ERROR: Unknown option."
+			;;
+	esac
+	done
+	shift $((OPTIND-1))
+
+    command ${simulation} -f "${format}" -- "$*"
     return $?
 }
 export -f subcommand
